d44c6c034704ef26f42c9df724d48ba1632696ab
DwiRatnaAmelia <124558154+DwiRatnaAmelia@users.noreply.github.com>
2023-02-12 11:44:06 +0700

    Create helloworld1.java

---
+++ tugas/helloworld1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hellowrold1;
+
+/**
+ *
+ * @author asus x453m
+ */
+public class Helloworld1 {
+ 
+    /**
+     * @pram args the command line arguments
+     */
+    public static void main(Straing[]  args){
+        System.out.println("Hello World");
+        
+        
+    }
+}
